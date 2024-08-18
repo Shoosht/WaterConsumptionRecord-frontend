92b0455a77e4c76c3ce74086acd3580cb46f94fd
ToniShoosh <toni.sustar0@gmail.com>
2024-08-18 23:25:38 +0200

    Initial commit with React app file cleanup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FirstScreen from './Pages/FirstScreen/FirstScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/FirstScreen/FirstScreen.js
@@ -0,0 +1,11 @@
+import './FirstScreen.css';
+
+function FirstScreen() {
+  return (
+    <div className="FirstScreen">
+        <p>Hello world</p>
+    </div>
+  );
+}
+
+export default FirstScreen;
