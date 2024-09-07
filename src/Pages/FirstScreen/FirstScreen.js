f38e289fac5edff0db66e19e20dd06bdc24320ad
ToniShoosh <toni.sustar0@gmail.com>
2024-09-07 20:19:20 +0200

    Updated login screen

--- src/Pages/FirstScreen/FirstScreen.js
+++ src/Pages/FirstScreen/FirstScreen.js
@@ -1,9 +1,66 @@
 import './FirstScreen.css';
+import githubLogo from '../../Components/github-mark.svg';
+import React, { useState } from 'react';
 
 function FirstScreen() {
+  const [LoginVisibility, setLoginVisibility] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginButtonClick = () => {
+    setLoginVisibility(false); 
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email, 'Password:', password);
+  };
+
   return (
-    <div className="FirstScreen">
-        <p>Hello world</p>
+    <div className="background">
+        <div className="title">
+          Water Consumption Record
+        </div>
+        <div className="login-button-centered">
+          {LoginVisibility ? (
+              <div className="login-button" onClick={handleLoginButtonClick}>
+                Login
+              </div>
+            ) : (
+              <div className="login-div">
+                <div className="login-centered">
+                  <div className="login-title">Login</div>
+                  <form className="login-form" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="email" className="login-text">Email</label>
+                      <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password" className="login-text">Password</label>
+                      <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <button type="submit" className="submit-button">Submit</button>
+                  </form>
+                </div>
+              </div>
+            )}
+        </div>
+        <div className="bottom-left">
+         <img height="50px" src={githubLogo} alt="GitHub Logo" />
+         <div className="bottom-left-text">Final Thesis Project</div>
+        </div>
     </div>
   );
 }
